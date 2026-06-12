2bf8c4fe9a8d1266f34ac1dcbeb7ab9d04852219
nithyasrid <nithyasridevasenan9706@gmail.com>
2026-06-12 15:31:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -2,22 +2,15 @@
 class Solution {
     static boolean armstrongNumber(int n) {
         // code here
-        int result  = 0;
-        int original = n;
-        int digit= 0;
-        int temp = n;
-        while(temp!=0){
-            temp/=10;
-            digit++;
-        }
-        temp = n;
-        while(temp!=0){
-            int remainder = temp %10;
-            result += Math.pow(remainder, digit);
-            temp/=10;
-        }
-        return result == original ;
-        
+       int k = String.valueOf(n).length();
+       int sum =0;
+       int num =n;
+       while(n>0){
+           int ld =n%10;
+           sum+=Math.pow(ld,k);
+           n/=10;
+       }
+       return sum == num;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
